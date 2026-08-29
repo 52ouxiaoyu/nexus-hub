9c316ddab1c53d8efd0bae8fe9f53939662b5209
John <52ouxiaoyu@gmail.com>
2026-08-29 21:44:02 +0800

    fix: map melonpult to correct drag ghost image so it can be seen when selected

--- pvz-web/js/InputManager.js
+++ pvz-web/js/InputManager.js
@@ -123,6 +123,9 @@ class InputManager {
             else if (type === 'twinsunflower') imgName = 'TwinSunflower/TwinSunflower1';
             else if (type === 'torchwood') imgName = 'Torchwood/Torchwood';
             else if (type === 'garlic') imgName = 'Garlic/Garlic';
+            else if (type === 'melonpult') imgName = 'MelonPult/MelonPult';
+            else if (type === 'wintermelon') imgName = 'MelonPult/MelonPult';
+            else if (type === 'cattail') imgName = 'Cattail/Cattail';
             
             this.dragGhost.style.backgroundImage = `url('assets/images/Plants/${imgName}.gif')`;
         }
